976bee3ff9e35c081127c5b4ad70342813d5d982
Gianluca-Basili <gianluc.basili@gmail.com>
2023-06-22 17:29:04 +0200

    add array with text e done

--- script.js
+++ script.js
@@ -3,10 +3,45 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-            title:'vue todolist',
+            title:'todolist',
+            tasks :[
+                {
+                    Text:'comprare il latte',
+                    done:false
+                },
+                {
+                    Text:'comprare il pane',
+                    done:false
+                },
+                {
+                    Text:'comprare il prezzemolo',
+                    done:false
+                },
+                {
+                    Text:'comprare la pizza',
+                    done:false
+                },
+                {
+                    Text:'comprare la nutella',
+                    done:false
+                },
+                {
+                    Text:'comprare la frutta',
+                    done:false
+                },
+                {
+                    Text:'comprare il pesce',
+                    done:false
+                },
+                {
+                    Text:'comprare la birra',
+                    done:false
+                }
+
+            ]
         }
     },
     methods:{
 
     }
-}).mount('#app')
+}).mount('#app');
